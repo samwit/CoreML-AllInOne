932879a3954fb138763706b73aadc2747f9e6574
Sam Witteveen <sam@samwitteveen.com>
2017-07-25 21:40:15 +0800

    Initial Commit

---
+++ CoreML-AllinOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreML-AllinOne
+//
+//  Created by Sam Witteveen on 25/7/17.
+//  Copyright © 2017 Sam Witteveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
